0186e797edf8af33c0ee443576a27e1f60dee607
Thierry <thierry.bouts@gmail.com>
2017-10-02 13:56:51 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+lafriperie
+==========
+
+A Symfony project created on October 1, 2017, 7:06 pm.
---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+// loads the jquery package from node_modules
+var $ = require('jquery');
+
+require('../css/main.scss');
