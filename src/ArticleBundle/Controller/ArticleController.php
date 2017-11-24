c28d9ce742e37298b4b239fa971fcd8f94e0451c
Thierry BOUTSIVONGSAKD <thierry.bouts@acensi.local>
2017-11-24 11:47:18 +0100

    add sorting input fields

--- src/ArticleBundle/Controller/ArticleController.php
+++ src/ArticleBundle/Controller/ArticleController.php
@@ -2,20 +2,26 @@
 
 namespace ArticleBundle\Controller;
 
-use ArticleBundle\Entity\Article;
-use ArticleBundle\Form\Type\ArticleType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use ArticleBundle\Entity\Article;
+use ArticleBundle\Entity\Image;
+use ArticleBundle\Form\Type\ArticleType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class ArticleController extends Controller
 {
     /**
      * @Route("/add", name="add")
      */
-    public function addArticle(Request $request)
+    public function add(Request $request)
     {
+        $session = new Session();
         $article = new Article();
+        $article->addImage(new Image());
 
         $form = $this->createForm(ArticleType::class, $article);
         $form->handleRequest($request);
@@ -24,19 +30,85 @@ class ArticleController extends Controller
             $article->setFosUserId($this->getUser());
             $article->setAdded(\DateTime::createFromFormat('Y-m-d H:i:s', date('Y-m-d H:i:s')));
 
-            //dump($this->getUser());
-            //dump($article);
-
             $em = $this->getDoctrine()->getManager();
             $em->persist($article);
             $em->flush();
 
-            return $this->render('article/add.html.twig', [
-                'form' => $form->createView(),
-            ]);
+            $session->getFlashBag()->add('success', 'Vôtre article a bien été enregistré');
+
+            return $this->redirectToRoute('showAll');
         }
+
         return $this->render('article/add.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+    /**
+     * @Route("/", options={"expose"=true}, name="showAll")
+     */
+    public function showAll(Request $request)
+    {
+        $repository = $this->getDoctrine()->getRepository('ArticleBundle:Article');
+        $articles = $repository->findAll();
+
+        return $this->render('article/showAll.html.twig', [
+            'articles' => $articles
+        ]);
+    }
+
+    /**
+     * @Route("/show-one/{id}", options={"expose"=true}, name="showOne")
+     */
+    public function showOne($id)
+    {
+        $repository = $this->getDoctrine()->getRepository('ArticleBundle:Article');
+        $article = $repository->find($id);
+
+        return $this->render('article/showOne.html.twig', [
+            'article' => $article,
+        ]);
+    }
+
+    public function addFilterToSidebarAction()
+    {
+        $repository = $this->getDoctrine()->getRepository('ArticleBundle:Article');
+        $brands = $repository->getBrands();
+        $categories = $repository->getCategories();
+        $sizes = $repository->getSizes();
+        $genders = $repository->getGenders();
+
+        return $this->render('sidebar.html.twig', [
+            'brands' => $brands,
+            'categories' => $categories,
+            'sizes' => $sizes,
+            'genders' => $genders
+        ]);
+    }
+
+    /**
+     * @Route("/ajax/request", options={"expose"=true}, name="ajax_request")
+     * @Method({"GET"})
+     */
+    public function ajaxRequestAction(Request $request)
+    {
+        $repository = $this->getDoctrine()->getRepository('ArticleBundle:Article');
+
+        if ($request->query->has('articles')) {
+            $getJsonArticles = $request->query->get('articles');
+            $selectedArticles = json_decode($getJsonArticles, true);
+
+            $articles = $repository->findByFilters($selectedArticles);
+        }
+
+        if ($request->query->has('search')) {
+            $getUserSearch = $request->query->get('search');
+            dump($getUserSearch);
+
+            $articles = $repository->findBySearchBar($getUserSearch);
+            dump($articles);
+        }
+
+        return new JsonResponse($articles);
     }
 }
--- src/ArticleBundle/Repository/ArticleRepository.php
+++ src/ArticleBundle/Repository/ArticleRepository.php
@@ -2,6 +2,9 @@
 
 namespace ArticleBundle\Repository;
 
+use ArticleBundle\Entity\Article;
+use Doctrine\DBAL\Connection;
+
 /**
  * ArticleRepository
  *
@@ -10,4 +13,103 @@ namespace ArticleBundle\Repository;
  */
 class ArticleRepository extends \Doctrine\ORM\EntityRepository
 {
+    public function getBrands()
+    {
+        $qb = $this->createQueryBuilder('a');
+        $qb
+            ->select('a.brand')
+            ->groupBy('a.brand');
+
+        return $qb->getQuery()->getResult();
+    }
+
+    public function getCategories()
+    {
+        $qb = $this->createQueryBuilder('a');
+        $qb
+            ->select('a.category')
+            ->groupBy('a.category');
+
+        return $qb->getQuery()->getResult();
+    }
+
+    public function getSizes()
+    {
+        $qb = $this->createQueryBuilder('a');
+        $qb
+            ->select('a.size')
+            ->groupBy('a.size');
+
+        return $qb->getQuery()->getResult();
+    }
+
+    public function getGenders()
+    {
+        $qb = $this->createQueryBuilder('a');
+        $qb
+            ->select('a.gender')
+            ->groupBy('a.gender');
+
+        return $qb->getQuery()->getResult();
+    }
+
+    public function findByFilters($articles)
+    {
+        dump($articles);
+        $qb = $this->createQueryBuilder('a');
+        $qb
+            ->select('a');
+
+        if (!empty($articles['brands'])) {
+            $qb
+                ->andWhere('a.brand IN (:brandFilter)')
+                ->setParameter('brandFilter', $articles['brands'], Connection::PARAM_STR_ARRAY);
+        }
+
+        if (!empty($articles['categories'])) {
+            $qb
+                ->andWhere('a.category IN (:categoryFilter)')
+                ->setParameter('categoryFilter', $articles['categories'], Connection::PARAM_STR_ARRAY);
+        }
+
+        if (!empty($articles['sizes'])) {
+            $qb
+                ->andWhere('a.size IN (:sizeFilter)')
+                ->setParameter('sizeFilter', $articles['sizes'], Connection::PARAM_STR_ARRAY);
+        }
+
+        if (!empty($articles['genders'])) {
+            $qb
+                ->andWhere('a.gender IN (:genderFilter)')
+                ->setParameter('genderFilter', $articles['genders'], Connection::PARAM_STR_ARRAY);
+        }
+
+        if (!empty($articles['prices']['from'])) {
+            $qb
+                ->andWhere('a.price >= (:priceFromFilter)')
+                ->setParameter('priceFromFilter', $articles['prices']['from'], Connection::PARAM_STR_ARRAY);
+        }
+
+        if (!empty($articles['prices']['to'])) {
+            $qb
+                ->andWhere('a.price <= (:priceToFilter)')
+                ->setParameter('priceToFilter', $articles['prices']['to'], Connection::PARAM_STR_ARRAY);
+        }
+
+        return $qb->getQuery()->getArrayResult();
+    }
+
+    public function findBySearchBar($userSearch)
+    {
+        $qb = $this->createQueryBuilder('a');
+        $qb
+            ->select('a')
+            ->where('a.name LIKE :search')
+            ->orWhere('a.description LIKE :search')
+            ->orWhere('a.category LIKE :search')
+            ->orWhere('a.brand LIKE :search')
+            ->setParameter('search', '%'.$userSearch.'%');
+
+        return $qb->getQuery()->getArrayResult();
+    }
 }
